2b0b4ed6e931afe62fd85d7114bf98419b4a8af0
pviictoor <pviictoor64@gmail.com>
2023-10-31 18:12:52 -0300

    primeiro commit

---
+++ Analog_Button.ino
@@ -0,0 +1,47 @@
+#include <Joystick.h>
+
+void setup() 
+
+ {pinMode(A3, INPUT);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Constant that maps the phyical pin to the joystick button.
+const int pinToButtonMap = 2;
+
+// Last state of the button
+int lastButtonState[11] = {0,0,0,0};
+
+
+void loop() {
+
+int pot = analogRead(A3);
+int mapped = map(pot,0,1023,0,255);
+{Joystick.setThrottle(mapped);}
+ 
+  // Read pin values
+  for (int index = 0; index < 11; index++)
+  {
+    int currentButtonState = !digitalRead(index + pinToButtonMap);
+    if (currentButtonState != lastButtonState[index])
+    {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  delay(50);
+}
